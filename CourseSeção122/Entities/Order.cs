4dfac7d6c747c82fbe3b4e8b0272a2a7078ce9a1
Rawann Silva <rawannsilveira@gmail.com>
2019-07-29 22:21:39 -0300

    Seção 122 do Curso C# Finalizada

---
+++ CourseSeção122/Entities/Order.cs
@@ -0,0 +1,63 @@
+﻿using CourseSeção122.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CourseSeção122.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public Client Client { get; set; }
+        public List<OrderItem> Item { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Item.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Item.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Item)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in Item)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ CourseSeção122/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseSeção122.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
